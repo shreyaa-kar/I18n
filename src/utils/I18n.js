53eca53a07e44546b465fbb1408b5f8d04058d75
shreyaa-kar <shreyakar444@gmail.com>
2024-03-30 13:48:09 +0530

    commit

---
+++ src/utils/I18n.js
@@ -0,0 +1,16 @@
+
+import languageConfig from "./lang.json";
+
+const I18n = (keyword, placeholder, value) => {
+    const lang = localStorage.getItem("lang") || "en";
+
+    const text = languageConfig[lang][keyword];
+
+    if(placeholder && value) {
+        return text.replace(placeholder,value);
+    }
+
+    return text;
+}
+
+export default I18n;
